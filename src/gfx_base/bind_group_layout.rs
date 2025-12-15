8f99c025a1083c5bea7a6458adfb6428fe686d78
zuiyu1998 <1542844298@qq.com>
2025-12-15 09:43:18 +0800

    feat: add BindGroupLayoutId

--- src/gfx_base/bind_group_layout.rs
+++ src/gfx_base/bind_group_layout.rs
@@ -1,5 +1,7 @@
 use wgpu::{BindGroupLayout as WgpuBindGroupLayout, BindGroupLayoutEntry};
 
+use crate::define_atomic_id;
+
 #[derive(Debug, Clone, Default, PartialEq, Eq, Hash)]
 pub struct BindGroupLayoutDescriptor {
     pub label: Option<String>,
@@ -19,17 +21,27 @@ impl GpuBindGroupLayout {
     }
 }
 
+define_atomic_id!(BindGroupLayoutId);
+
 #[derive(Clone, Debug)]
 pub struct BindGroupLayout {
+    id: BindGroupLayoutId,
     value: GpuBindGroupLayout,
 }
 
 impl BindGroupLayout {
     pub fn new(value: GpuBindGroupLayout) -> Self {
-        BindGroupLayout { value }
+        BindGroupLayout {
+            value,
+            id: BindGroupLayoutId::new(),
+        }
     }
 
     pub fn value(&self) -> &GpuBindGroupLayout {
         &self.value
+    }
+
+    pub fn id(&self) -> BindGroupLayoutId {
+        self.id
     }
 }
